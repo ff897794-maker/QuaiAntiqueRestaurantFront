1c6b2564cae62ef84402568d840c61b14abc9cec
François Garnier <ff897794@gmail.com>
2026-09-09 11:52:33 +0200

    auth-route-patch

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,9 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "Galerie", "/pages/galerie.html"),
-  new Route("/signIn", "Connexion", "/pages/signIn.html"),
-  new Route("/signUp", "Inscription", "/pages/signUp.html"),
+  new Route("/signIn", "Connexion", "/pages/auth/signIn.html"),
+  new Route("/signUp", "Inscription", "/pages/auth/signUp.html"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
