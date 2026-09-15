d2d99f946ad53f93caa6b9fda4eb0faf7b342ec9
François Garnier <ff897794@gmail.com>
2026-09-15 16:13:56 +0200

    Sonar-fix

--- js/auth/signIn.js
+++ js/auth/signIn.js
@@ -11,9 +11,12 @@ function checkCredentials() {
   let myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
+  let checkedUserName = sanitizeHtml(dataForm.get("email"));
+  let checkedPassword = sanitizeHtml(dataForm.get("password"));
+
   let raw = JSON.stringify({
-    username: dataForm.get("email"),
-    password: dataForm.get("password"),
+    username: checkedUserName,
+    password: checkedPassword,
   });
 
   const requestOptions = {
